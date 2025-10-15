741d624c59b3308d484a3075d593e27220509a8e
Kirill <kandriyanoff@yandex.ru>
2025-10-15 22:24:16 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        Author jr = new Author("Joanne", "Rowling");
+        Author ar = new Author("Ayn", "Rand");
+
+        Book harryPotterPart1 = new Book("Harry Potter", jr, 1996);
+        Book atlasShrugged = new Book("atlasShrugged", ar, 1957);
+
+        harryPotterPart1.setYear(1997);
+
     }
 }
